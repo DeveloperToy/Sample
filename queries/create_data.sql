b34fb5b197f6d7d210d13b0d08d52cd8600e935e
yokodakeita <37468879+yokodakeita@users.noreply.github.com>
2018-04-08 01:52:11 +0900

    Create create_data.sql

---
+++ queries/create_data.sql
@@ -0,0 +1,15 @@
+/*
+ * データ生成クエリ.
+ */
+
+# APIキーマスタ
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'M2h)CAve' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'eM8~pQtH' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 't|6M!D.X' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'j4/QkDMp' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'Q!g68cuS' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'v#Y2LQq~' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( '%Rs8Yr3y' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'N~7sq|X)' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( 'wL7*k4ZV' , cast('2019-04-07' as date) );
+insert into M_API_KEY(  API_KEY , EXPIRATION_DATE ) value ( '&Z5cb_#k' , cast('2019-04-07' as date) );
